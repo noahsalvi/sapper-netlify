d45b7f5aee377bdb50d0efd83c450a9ec2cfeab3
Noah Salvi <noah.salvi@ti8m.ch>
2021-03-05 23:00:43 +0100

    remove node_modules

--- bin.js
+++ bin.js
@@ -2,18 +2,16 @@
 const child_process = require("child_process");
 const path = require("path");
 const fs = require("fs");
-const { exit } = require("process");
 
 const args = process.argv.slice(2, process.argv.length);
-const currentDirectoryName = path.basename(process.cwd());
-const projectRoot = args[0] === "." ? "./" : args[0];
-// const projectRoot = path.join(__dirname, "..");
-// const projectRoot = "my-app/";
+const projectPath = args[0] === "." ? "./" : args[0];
 const assets = path.join(__dirname, "assets");
 
-if (!projectRoot) {
-  console.log("😓 You need to specify a name: e.g. npx sapper-netlify my-app");
-  exit(1);
+if (!args[0]) {
+  console.log(
+    "😓 You need to specify a path: e.g. npx sapper-netlify my-app 😓"
+  );
+  process.exit();
 }
 
 run();
@@ -39,32 +37,32 @@ async function run() {
 }
 
 function installSapper() {
-  const projectName = args[0] === "." ? currentDirectoryName : args[0];
-
-  return exec(`npx degit "sveltejs/sapper-template#rollup" ${args[0]}`);
+  return exec(
+    `npx degit "sveltejs/sapper-template#rollup" ${args[0]} ${args[1] ?? ""}`
+  );
 }
 
 function addBuildNetlifyScript() {
   const buildNetlify = path.join(assets, "buildNetlify.js");
-  const scriptsPath = path.join(projectRoot, "scripts");
+  const scriptsPath = path.join(projectPath, "scripts");
   return exec(`cp ${buildNetlify} ${scriptsPath}`);
 }
 
 function addRedirects() {
   const _redirects = path.join(assets, "_redirects");
-  const staticPath = path.join(projectRoot, "static");
+  const staticPath = path.join(projectPath, "static");
   return exec(`cp ${_redirects} ${staticPath}`);
 }
 
 function addNetlifyToml() {
   const netlifyToml = path.join(assets, "netlify.toml");
-  const destination = projectRoot;
+  const destination = projectPath;
 
   exec(`cp ${netlifyToml} ${destination}`);
 }
 
 function modifyServer() {
-  const serverFile = path.join(projectRoot, "src/server.js");
+  const serverFile = path.join(projectPath, "src/server.js");
   const newServerFile = path.join(assets, "server.js");
 
   exec(`cp ${newServerFile} ${serverFile}`);
@@ -72,7 +70,7 @@ function modifyServer() {
 
 function addRenderFunction() {
   const renderFnPath = path.join(assets, "/render");
-  const destPath = path.join(projectRoot, "/functions");
+  const destPath = path.join(projectPath, "/functions");
   exec(`mkdir -p ${destPath} && cp -R ${renderFnPath} $_`);
 }
 
@@ -90,7 +88,7 @@ function exec(command) {
 }
 
 function addPkgDependencies() {
-  const pkgJSONPath = path.join(projectRoot, "package.json");
+  const pkgJSONPath = path.join(projectPath, "package.json");
   const packageJSON = JSON.parse(fs.readFileSync(pkgJSONPath, "utf8"));
   packageJSON.dependencies = Object.assign(packageJSON.dependencies, {
     "serverless-http": "^2.7.0",
@@ -106,7 +104,7 @@ function addPkgDependencies() {
 }
 
 function addPkgScript() {
-  const pkgJSONPath = path.join(projectRoot, "package.json");
+  const pkgJSONPath = path.join(projectPath, "package.json");
   const packageJSON = JSON.parse(fs.readFileSync(pkgJSONPath, "utf8"));
   packageJSON.scripts = Object.assign(packageJSON.scripts, {
     "build-netlify": "node scripts/buildNetlify",
